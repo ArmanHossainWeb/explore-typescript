3a267fa062b72ebf483443fc28c4a7e5567d7a1e
Your Name <you@example.com>
2025-11-26 17:46:08 +0600

    type interface

--- src/Interface.ts
+++ src/Interface.ts
@@ -3,21 +3,44 @@ type User = {
     age:number
 }
 
+interface Iuser {
+    name: string,
+    age: number
+}
+
 type Role = {
     role: "admin" | "user"
 }
-
 
 type UserWithRole = User & Role ;
 
 
 
-const user1 : UserWithRole ={
-    name:"arman",
-    age:100,
-    role: "admin}
-
-const user2 : User ={
-    name:"tanvir",
-    age:102
+interface IUserWithRole extends Iuser {
+    role: "admin" | "user";
 }
+
+
+
+const user1 :IUserWithRole = {
+    name: "arman",
+    age: 12,
+    role: 'admin'
+}
+const user2 :Iuser = {
+    name: "Hossain",
+    age: 15,
+}
+
+
+type Friends = string[];
+
+
+interface IFriends {
+    [index: number] : string;
+}
+
+const friends : IFriends = ["A", "B", "C"];
+ 
+
+
