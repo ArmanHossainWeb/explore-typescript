e5c3168e7a2f12afb7948b6a31ac715d3056e69d
Arman Hossain <armanhossain0477@gmail.com>
2025-12-01 20:52:51 +0600

    learning enum

---
+++ src/enum.ts
@@ -0,0 +1,17 @@
+// const UserRole = "Admin" | "Editor" | "Viewer";
+
+enum UserRole {
+    Admin = "Admin",
+    Editor = "Editor",
+    Viewer = "Viewer",
+}
+
+const canEdit = (role: UserRole) => {
+ if (role === UserRole.Admin || role === UserRole.Editor) {
+    return true
+ }
+ else return false
+}
+
+const isEditPermissable = canEdit(UserRole.Admin)
+console.log(isEditPermissable)
--- src/keyOfContraint.ts
+++ src/keyOfContraint.ts
@@ -1,30 +1,79 @@
-function addNormal(num1:number, num2: number) : number {
-    return num1 + num2
+type RichPeoplesVehicle = {
+    car: string; //kye: value
+    bike: string;
+    cng: string;
 }
 
+type MyVehicle1 = "bike" | "car" | "cng";
+type MyVehicle2 = keyof RichPeoplesVehicle;
 
-
-const addArrow = (num1: number, num2 : number) :number => num1 + num2 ;
-
-
-
-
-
-addNormal(2, 3)
-addArrow(3,5)
+const myVehicle: MyVehicle1 = "bike";
 
 
 
+type User = {
+    id: number;
+    name: string;
+    address: {
+        city: string
+    };
+}
 
-const poorUser = {
+const user : User = {
+    id: 222,
     name:"arman",
-    balance: 0,
-    addBalance(value:number) {
-        const totalBalance = this.balance + value;
-        return totalBalance;
+    address: {
+        city: "dhaka"
     }
+
 }
 
+const userId = user["id"];
+const userName = user["name"];
+const userAddess = user["address"];
+
+// console.log({userId, userName, userAddess});
 
 
-console.log(poorUser.addBalance(10000000))
+const getPropertyFromObj = <X>(obj: X, key: keyof X) => {
+    return obj[key]
+} 
+
+const result = getPropertyFromObj(user, "name");
+console.log(result)
+
+
+
+
+
+
+const product = {
+    brand: "HP"
+}
+
+const result2 = getPropertyFromObj(product, "brand")
+console.log(result2)
+
+
+
+
+
+
+
+const student = {
+    id: 123,
+    class:"four"
+}
+
+const result3 = getPropertyFromObj(student , "id")
+console.log(result3)
+ 
+
+
+
+
+
+
+
+
+
