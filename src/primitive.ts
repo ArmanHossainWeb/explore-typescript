5e50c4d091daba7b0db0e772783e739f9bfaf99b
Arman Hossain <armanhossain0477@gmail.com>
2025-12-14 10:14:55 +0600

    feat: primitive.ts file

---
+++ src/primitive.ts
@@ -0,0 +1,25 @@
+// JS - TS
+// string, number , boolean, undefined, null 
+
+
+// TS: never, unknown, void 
+
+
+let userName : string = "armanHossain";
+userName = "12";
+
+
+let userId : number = 1234;
+let str: string = userId.toString()
+console.log(userId)
+
+
+let isAdmin :boolean = false;
+isAdmin = true;
+
+
+
+
+let x : undefined = undefined;
+x = 5
+x = "five"
