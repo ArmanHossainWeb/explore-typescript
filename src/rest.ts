2ac7e5f37ad28279dbdfeab703f812789a013047
Arman Hossain <armanhossain0477@gmail.com>
2025-12-14 17:47:44 +0600

    implement rest operation

---
+++ src/rest.ts
@@ -0,0 +1,7 @@
+const sendInvite = (friend1:string, friend2: string, friend3:string) => {
+  console.log(`send invite ${friend1}`)
+  console.log(`send invite ${friend2}`)
+  console.log(`send invite ${friend3}`)
+}
+
+sendInvite("shakhawat", "hossain", "arman")
