15eaa8b19c41838f201aa33d65abd8934c611051
Arman Hossain <armanhossain0477@gmail.com>
2025-12-17 16:51:24 +0600

    solved some generics

--- src/generics.ts
+++ src/generics.ts
@@ -3,14 +3,13 @@
 
 type Generics<T> = Array<T>;
 
-
 const friends: Generics<string> = ["arman", "hossain", "shakhawat"];
-
 const rollNumber: Generics<number> = [8, 9, 7];
-
 const isEligable: Generics<boolean> = [true, false, false, true];
 
-const UserList: Generics<{ name: string, age: number }> = [
+
+type userType = { name: string, age: number }
+const UserList: Generics<userType> = [
     {
         name: "arman",
         age: 20
@@ -23,10 +22,19 @@ const UserList: Generics<{ name: string, age: number }> = [
 
 
 
-type coordinates<X, Y> = [X, Y];
 
+
+
+type coordinates<X, Y> = [X, Y];
 const coordinates1: coordinates<number, number> = [20, 40];
 const coordinates2: coordinates<string, string> = ["20", "40"];
+
+
+
+
+
+type User<X,Y> = [X,Y]
+const user1 : User<string, number> =  ["arman", 25]
 
 
 
