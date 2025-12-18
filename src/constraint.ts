92a60909537de0a511c7c4baed63d1b373716f65
Arman Hossain <164849774+ArmanHossainWeb@users.noreply.github.com>
2025-12-18 13:03:23 +0000

    added some file

--- src/constraint.ts
+++ src/constraint.ts
@@ -1,6 +1,11 @@
-const addStudent = <T extends { id: number; name: string }>(studentInfo: T) => {
+type student = { id: number; name: string }
+
+const addStudent = <T extends student >(studentInfo: T) => {
     return { Course: "Programming", ...studentInfo };
 };
 
 const s1 = { id: 123, name: "Dev1", useLinux: true };
 const result = addStudent(s1);
+
+const s2 = {name:"hello", id:123}
+const result1 = addStudent(s2)
